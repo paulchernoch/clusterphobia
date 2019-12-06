98c6111df0c5320f39bf3ddeb6523e25638a5616
paulchernoch <paul.chernoch@yahoo.com>
2019-12-05 21:45:10 -0500

    Speed up msb

--- src/clustering/msb.rs
+++ src/clustering/msb.rs
@@ -23,20 +23,33 @@ pub trait MostSignificantBit {
     fn msb(self) -> usize;
 }
 
+#[inline]
+/// Return whether floor(log2(x))!=floor(log2(y))
+/// with zero for false and 1 for true, because this came from C!
+fn ld_neq(x : u64, y : u64) -> u64 {
+    let neq = (x^y) > (x&y);
+    if neq { 1 } else { 0 }
+}
+
 impl MostSignificantBit for u64 {
     #[inline]
     fn msb(self) -> usize {
-        // Bisection guarantees performance of O(Log B) where B is number of bits in integer.
-        let mut high = 63_usize;
-        let mut low = 0_usize;
-        while (high - low) > 1
-        {
-            let mid = (high+low)/2;
-            let mask_high = (1 << high) - (1 << mid);
-            if (mask_high & self) != 0 { low = mid; }
-            else { high = mid; }
-        }
-        low
+        // This algorithm found on pg 16 of "Matters Computational" at  https://www.jjj.de/fxt/fxtbook.pdf
+        // It avoids most if-branches and has no looping.
+        // Using this instead of Bisection and looping shaved off 1/3 of the time.
+        const MU0 : u64 = 0x5555555555555555; // MU0 == ((-1UL)/3UL) == ...01010101_2
+        const MU1 : u64 = 0x3333333333333333; // MU1 == ((-1UL)/5UL) == ...00110011_2
+        const MU2 : u64 = 0x0f0f0f0f0f0f0f0f; // MU2 == ((-1UL)/17UL) == ...00001111_2
+        const MU3 : u64 = 0x00ff00ff00ff00ff; // MU3 == ((-1UL)/257UL) == (8 ones)
+        const MU4 : u64 = 0x0000ffff0000ffff; // MU4 == ((-1UL)/65537UL) == (16 ones)
+        const MU5 : u64 = 0x00000000ffffffff; // MU5 == ((-1UL)/4294967297UL) == (32 ones)
+        let r : u64 = ld_neq(self, self & MU0)
+        + (ld_neq(self, self & MU1) << 1)
+        + (ld_neq(self, self & MU2) << 2)
+        + (ld_neq(self, self & MU3) << 3)
+        + (ld_neq(self, self & MU4) << 4)
+        + (ld_neq(self, self & MU5) << 5);
+        r as usize
     }
 }
 
